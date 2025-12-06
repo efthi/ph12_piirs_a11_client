5a592f216e02ebb255d5b08effb594ee5c825405
efthaqur <efthaqur@gmail.com>
2025-12-07 03:23:07 +0600

    Configure: Firebase ready for client

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
-import Banner from '../components/home/Banner';
-import LatestIssues from '../components/home/LatestIssues';
-import Features from '../components/home/Features';
-import HowItWorks from '../components/home/HowItWorks';
+import Banner from "../components/home/Banner";
+import LatestIssues from "../components/home/LatestIssues";
+import Features from "../components/home/Features";
+import HowItWorks from "../components/home/HowItWorks";
+import { auth } from "../Firebase/firebase.config";
 
 const Home = () => {
   return (

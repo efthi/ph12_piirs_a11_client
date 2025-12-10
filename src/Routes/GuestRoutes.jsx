760e930b83dd75fade870ba65af700eea0d778c7
efthaqur <efthaqur@gmail.com>
2025-12-10 21:00:58 +0600

    Update: GuestRoute and Private Route

--- src/Routes/GuestRoutes.jsx
+++ src/Routes/GuestRoutes.jsx
@@ -4,9 +4,11 @@ import { Navigate, useLocation } from "react-router";
 import Loader from "../components/shared/Loader";
 
 const GuestRoutes = ({ children }) => {
-  const { user, loading } = useAuth();
-  let location = useLocation();
-
+  const { user, authload } = useAuth();
+  let backto = useLocation();
+  if (authload) {
+    return <Loader></Loader>;
+  }
   if (user) {
     const backto = location.state?.from?.pathname || "/";
     return <Navigate to={backto} replace></Navigate>;
--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import useAuth from '../hooks/useAuth';
-import { Navigate, useLocation } from 'react-router';
-import Loader from '../components/shared/Loader';
+import React from "react";
+import useAuth from "../hooks/useAuth";
+import { Navigate, useLocation } from "react-router";
+import Loader from "../components/shared/Loader";
 
-const PrivateRoutes = ({children}) => {
-    
-    const {user, loading} = useAuth();
-    let location = useLocation();
-    if(user){
-        return children;
-    }
-    if(loading){
-        return <Loader></Loader>;
-    }
-    return <Navigate state={location?.pathname} to="/login"></Navigate>;
+const PrivateRoutes = ({ children }) => {
+  const { user, authload } = useAuth();
+  let location = useLocation();
+
+  // ১) আগে loading check
+  if (authload) {
+    return <Loader />;
+  }
+
+  // ২) তারপর user check
+  if (user) {
+    return children;
+  }
+
+  // ৩) logged in না হলে login এ পাঠাও
+  return (
+    <Navigate
+      to="/login"
+      state={{ from: location }}
+      replace
+    />
+  );
 };
 
 export default PrivateRoutes;
